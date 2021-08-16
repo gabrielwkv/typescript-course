ec1c7df7b23be80557bf0f40b09fd4cc4106c1dd
Gabriel M. dos Santos <gabrielwkv@MacBook-Pro-de-Gabriel.local>
2021-08-16 17:21:03 -0500

    using implements and extends

--- src/object-typing.ts
+++ src/object-typing.ts
@@ -27,3 +27,39 @@ let users: User[];
 // # as typescript you can also work with optional properties
 // # do you enter a ? before the attribute
 // # as was done in the example above in function showCity
+
+// # it is possible to leave properties locked for change, so they are just readonly
+interface NewUser {
+  readonly name: string;
+}
+
+let user: NewUser = {
+  name: "Gabriel Maciel",
+};
+
+// user.name = "João"; # error: cannot assign to 'name' because it is a read-only property
+
+// # working with implements and extends
+interface Vehicle {
+  wheels: number;
+  speed: () => void;
+  brake?: () => void;
+}
+
+// interface Motorcycle {
+//   helmet: boolean;
+//   cram: () => void;
+// }
+
+// let bross: Motorcycle
+class CreateVehicle implements Vehicle {
+  wheels: number;
+
+  constructor(wheels: number) {
+    this.wheels = wheels;
+  }
+
+  speed() {
+    console.log("speed");
+  }
+}

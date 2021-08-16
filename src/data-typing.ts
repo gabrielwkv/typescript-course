09dbed20de7ed6a28d9c839a9274e6b3fdcbc16a
Gabriel M. dos Santos <gabrielwkv@MacBook-Pro-de-Gabriel.local>
2021-08-16 11:44:51 -0500

    exploring data typing

---
+++ src/data-typing.ts
@@ -0,0 +1,10 @@
+// type data
+let message: string = "Hello World";
+
+console.log(message.toUpperCase());
+
+let total: number = 10.515;
+
+console.log(total.toFixed(1));
+
+let isOpen: boolean = false;

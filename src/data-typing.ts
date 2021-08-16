2c7d0002b615978c087b3880c5a4b3adaa723dc9
Gabriel M. dos Santos <gabrielwkv@MacBook-Pro-de-Gabriel.local>
2021-08-16 11:55:28 -0500

    typing any, void and never

--- src/data-typing.ts
+++ src/data-typing.ts
@@ -8,3 +8,22 @@ let total: number = 10.515;
 console.log(total.toFixed(1));
 
 let isOpen: boolean = false;
+
+// suppose that when the stock runs out it should be null
+// let stock: any = 89; # that's why using any, but it's not advisable
+let stock: number | null = 89;
+stock = null; // # the correct way to use numeric and null
+
+// # void is used for functions that have no return.
+function showInformation(name: string = "Gabriel"): void {
+  console.log(name);
+}
+
+showInformation();
+
+// # never is often used in functions that have infinite loops or exceptions.
+function error(): never {
+  throw new Error("Error");
+}
+
+error();
